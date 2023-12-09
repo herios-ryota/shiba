8b757d90d9c2b7c185e8d6536187473874bdd957
ryo-t <ryota_saki_86@ezweb.ne.jp>
2023-12-10 04:44:56 +0900

    config

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: 'docs',
-  assetsDir: './',
-  publicPath: './',
-  css: {
-    loaderOptions: {
-      scss: {
-        data: '@import "./src/styles/common/common.scss";'
-      }
-    }
-  }
+  publicPath: './'
 });
